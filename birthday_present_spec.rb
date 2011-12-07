25919cc0cba7ceb4c42e202bc11f705cecc5d250
Steve Tooke <steve@heavi.es>
2011-12-07 07:35:38 +0000

    First non-east spec for a female birthday.

---
+++ birthday_present_spec.rb
@@ -0,0 +1,17 @@
+require 'date'
+
+module NonEast
+  describe 'sending a birthday present to a customer' do
+    context 'non-east customers' do
+
+      it 'sends flowers to a female customer on their birthday' do
+        today = Date.parse("2011-10-09")
+        today_20_years_ago = Date.parse("1991-10-09")
+        customer = double(sex: :female, date_of_birth: today_20_years_ago)
+
+        selector = BirthdayPresentSelector.new(customer, today)
+        selector.present.should == :flowers
+      end
+    end
+  end
+end
