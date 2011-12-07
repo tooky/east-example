7adfadba2ad009e066cf93821427685decc59dd4
Steve Tooke <steve@heavi.es>
2011-12-07 07:59:05 +0000

    Ensure men get nothing unless its their birthday as well.

--- birthday_present_spec.rb
+++ birthday_present_spec.rb
@@ -3,6 +3,7 @@ require 'date'
 module NonEast
   describe 'sending a birthday present to a customer' do
     context 'non-east customers' do
+      let(:tomorrow_20_years_ago) { Date.parse("1991-10-10") }
       let(:today_20_years_ago) { Date.parse("1991-10-09") }
       let(:today) { Date.parse("2011-10-09") }
 
@@ -21,8 +22,14 @@ module NonEast
       end
 
       it 'sends nothing to a female customer when it is not their birthday' do
-        tomorrow_20_years_ago = Date.parse("1991-10-10")
         customer = double(sex: :female, date_of_birth: tomorrow_20_years_ago)
+
+        selector = BirthdayPresentSelector.new(customer, today)
+        selector.present.should == :none
+      end
+
+      it 'sends nothing to a male customer when it is not their birthday' do
+        customer = double(sex: :male, date_of_birth: tomorrow_20_years_ago)
 
         selector = BirthdayPresentSelector.new(customer, today)
         selector.present.should == :none
@@ -37,12 +44,14 @@ module NonEast
     end
 
     def present
-      if birthday_today?
-        return :cufflinks if @customer.sex == :male
-        :flowers
-      else
-        :none
-      end
+      return selected_present if birthday_today?
+      :none
+    end
+
+    private
+    def selected_present
+      return :cufflinks if @customer.sex == :male
+      :flowers
     end
 
     def birthday_today?
