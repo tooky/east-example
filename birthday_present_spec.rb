89453a0ef3b55028616b5d908ec8ac541fa2c565
Steve Tooke <steve@heavi.es>
2011-12-07 07:48:36 +0000

    Extract lets for dates.

--- birthday_present_spec.rb
+++ birthday_present_spec.rb
@@ -3,10 +3,10 @@ require 'date'
 module NonEast
   describe 'sending a birthday present to a customer' do
     context 'non-east customers' do
+      let(:today_20_years_ago) { Date.parse("1991-10-09") }
+      let(:today) { Date.parse("2011-10-09") }
 
       it 'sends flowers to a female customer on their birthday' do
-        today = Date.parse("2011-10-09")
-        today_20_years_ago = Date.parse("1991-10-09")
         customer = double(sex: :female, date_of_birth: today_20_years_ago)
 
         selector = BirthdayPresentSelector.new(customer, today)
@@ -14,8 +14,6 @@ module NonEast
       end
 
       it 'sends cufflinks to a male customer on their birthday' do
-        today = Date.parse("2011-10-09")
-        today_20_years_ago = Date.parse("1991-10-09")
         customer = double(sex: :male, date_of_birth: today_20_years_ago)
 
         selector = BirthdayPresentSelector.new(customer, today)
